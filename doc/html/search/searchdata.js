b8a5a5cb86e9807d6760e753206ed83692ccc03f
Marcos Horro <marcos.horro@udc.es>
2018-11-27 16:54:22 +0100

    Updated Doxygenfiles

--- doc/html/search/searchdata.js
+++ doc/html/search/searchdata.js
@@ -1,30 +1,36 @@
 var indexSectionsWithContent =
 {
   0: "_ipr",
-  1: "pr",
-  2: "ip",
-  3: "p",
+  1: "p",
+  2: "pr",
+  3: "ip",
   4: "_p",
-  5: "p"
+  5: "p",
+  6: "_p",
+  7: "p"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files",
-  2: "functions",
-  3: "variables",
-  4: "defines",
-  5: "pages"
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "defines",
+  7: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Files",
-  2: "Functions",
-  3: "Variables",
-  4: "Macros",
-  5: "Pages"
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Macros",
+  7: "Pages"
 };
 
---
+++ doc/html/search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pw_5fthread_5finfo_5ft',['PW_thread_info_t',['../papi__wrapper_8h.html#ae91b02afae7c4d8ee1e8e228f9ecd91c',1,'papi_wrapper.h']]]
+];
