07d37dfe9d07846bb8305f7d1830abd579c483e1
Marcos Horro <marcos.horro@udc.es>
2020-05-26 19:00:19 +0200

    VM does not support PAPI, allegedly, so I will only be able to compile, not to test... Hope this work someday, it is already done. Due to this, code coverage is also not supported

--- build_tests.sh
+++ build_tests.sh
@@ -5,7 +5,10 @@ set -eo pipefail
 testing=$1
 
 # Install PAPI library
-git clone https://bitbucket.org/icl/papi.git && cd papi/src && ./configure && make && make install && cd ../..
+#git clone https://bitbucket.org/icl/papi.git && cd papi/src && ./configure && make && sudo make install && cd ../..
+
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+sudo bash -c "echo -1 > /proc/sys/kernel/perf_event_paranoid"
 
 mkdir -p build && cd build
 
@@ -17,6 +20,6 @@ cmake --build . --config Debug -- -j1
 
 if [[ $testing = "testing" ]]; then
     # Test
-    ctest --output-on-failed
-    OMP_NUM_THREADS=1 ctest --rerun-failed
+    LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ctest --output-on-failed
+    LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH OMP_NUM_THREADS=1 ctest --rerun-failed
 fi
