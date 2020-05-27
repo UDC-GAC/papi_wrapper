83dd02307d22928525518faed64aa81e58da0f15
Marcos Horro <marcos.horro@udc.es>
2020-05-27 09:47:45 +0200

    New release ready

--- build_tests.sh
+++ build_tests.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 testing=$1
 
 # Install PAPI library
-#git clone https://bitbucket.org/icl/papi.git && cd papi/src && ./configure && make && sudo make install && cd ../..
+git clone https://bitbucket.org/icl/papi.git && cd papi/src && ./configure && make && sudo make install && cd ../..
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 sudo bash -c "echo -1 > /proc/sys/kernel/perf_event_paranoid"
@@ -20,6 +20,6 @@ cmake --build . --config Debug -- -j1
 
 if [[ $testing = "testing" ]]; then
     # Test
-    LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ctest --output-on-failed
+    LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ctest --output-on-failure
     LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH OMP_NUM_THREADS=1 ctest --rerun-failed
 fi
